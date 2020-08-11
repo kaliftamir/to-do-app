f68a2795fae54343244429b5403ad11684636757
Tamir <kaliftamir@gmail.com>
2020-08-11 12:23:44 +0300

    Start

---
+++ src/components/TodosPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './TodosPage/Todo';
+
+
+function TodosPage() {
+
+
+
+    return (
+       <div className="container">
+           <h2>Todos</h2>
+           <input placeholder="What's next?"/>
+           <Todo/>
+       </div>
+    )
+}
+
+export default TodosPage;
---
+++ src/data-model/TodoModel.js
@@ -0,0 +1,11 @@
+
+
+class TodoModel {
+    constructor(text) {
+        this.brand = text;
+
+    }
+
+}
+
+export default TodoModel;
